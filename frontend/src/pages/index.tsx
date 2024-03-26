ec14ab040e721c0eb4ad41785923f07046fb2af3
tuvshuu <tuvshee2k@gmail.com>
2024-03-26 21:52:39 +0800

    unuudriin shaasan yumnuud
    mar.26

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,8 @@
+import Footer from "@/components/layout/footer";
 export default function Home() {
-  return (
-    <>
-      <p>Hello</p>
-    </>
-  );
+     return (
+          <>
+               <Footer />
+          </>
+     );
 }
