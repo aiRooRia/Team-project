84855eba9e3f4821cabb8858b45a1f8d46eb5256
aiRoRia <ariguun10@gmail.com>
2024-03-26 21:22:36 -0700

    3sariin26 ariguun

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,29 @@
+import { Box, Button, Container, TextField } from "@mui/material";
+import { green } from "@mui/material/colors";
+
+const Home = () => {
+  return (
+    <>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "1440px",
+          height: "100vh",
+        }}
+      >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
+        <Button
+          variant="text"
+          sx={{ width: 385, height: 48, color: "#18BA51", border: 1 }}
+        >
+          Бүртгүүлэх
+        </Button>
+      </Container>
+    </>
+  );
+};
+export default Home;
