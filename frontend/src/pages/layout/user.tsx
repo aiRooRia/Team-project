7092b2a5dfb406d0509e046a661381fcc8e7d22c
Senge21 <Ssenge1515@gmail.com>
2024-03-29 20:17:32 +0800

    done user profile

--- frontend/src/pages/layout/user.tsx
+++ frontend/src/pages/layout/user.tsx
@@ -4,10 +4,10 @@ import { UserProfile } from "@/components/layout/header/UserProfile";
 import { Box } from "@mui/material";
 export default function Home() {
   return (
-    <Box sx={{ width: "100%", height: "100vh", position: "relative" }}>
+    <>
       <Header></Header>
       <UserProfile />
       <Footer />
-    </Box>
+    </>
   );
 }
