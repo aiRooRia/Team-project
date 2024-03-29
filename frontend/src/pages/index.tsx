07c8d63f82cc4209d90ac03e86ba5169dc654048
Senge21 <Ssenge1515@gmail.com>
2024-03-29 20:11:05 +0800

    user profile done

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
 import { Header } from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
-import { UserProfile } from "@/components/layout/header/UserProfile";
+
 export default function Home() {
   return (
     <>
       <Header></Header>
-      <UserProfile />
+
       <Footer />
     </>
   );
