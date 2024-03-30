9c833099cb59642943df1cde440039ea41b66104
aiRoRia <ariguun10@gmail.com>
2024-03-30 15:43:06 -0700

    3/30 formik barg duussan baih Ariguun

--- frontend/src/components/signup/index.tsx
+++ frontend/src/components/signup/index.tsx
@@ -7,37 +7,38 @@ import {
   OutlinedInput,
   InputAdornment,
   IconButton,
+  Checkbox,
 } from "@mui/material";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { FormikProvider, useFormik } from "formik";
-import { signUpSchema } from "@/components/signup/validationSchema";
-import { useState, useContext, useEffect } from "react";
-import { UserContext } from "../utils/context";
-import { useRouter } from "next/router";
+import { signUpSchema, FromValues } from "@/components/signup/validationSchema";
+import { useEffect } from "react";
 import * as React from "react";
 
-export const Signup = () => {
-  const [warningMessage, setWarningMessage] = useState("");
-  const { signUpUserInfo, setSignUpUserInfo } = useContext(UserContext);
-  const { push } = useRouter();
-  const formikSignUp = useFormik({
+export const Signup: React.FC = () => {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  const [isChecked, setIsChecked] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+  const formikSignUp = useFormik<FromValues>({
     initialValues: {
-      name: "",
-      password: "",
-      email: "",
-      location: "",
-      rePassword: "",
+      name: null,
+      location: null,
+      password: null,
+      email: null,
+      rePassword: null,
     },
     validationSchema: signUpSchema,
     onSubmit: async (values) => {
       console.log(values);
     },
   });
-  useEffect(() => {}, [warningMessage]);
 
-  const [showPassword, setShowPassword] = React.useState(false);
+  useEffect(() => {}, []);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleMouseDownPassword = (
@@ -45,6 +46,7 @@ export const Signup = () => {
   ) => {
     event.preventDefault();
   };
+
   return (
     <>
       <Stack
@@ -55,13 +57,12 @@ export const Signup = () => {
         sx={{ padding: "32px" }}
       >
         <Typography
-          sx={{ fontWeight: "700px", fontSize: "28px", textAlign: "center" }}
+          sx={{ fontWeight: "700", fontSize: "28px", textAlign: "center" }}
         >
           Бүртгүүлэх
         </Typography>
         <FormikProvider value={formikSignUp}>
           <form onSubmit={formikSignUp.handleSubmit}>
-            {" "}
             <Stack
               width={"100%"}
               direction="column"
@@ -71,27 +72,49 @@ export const Signup = () => {
             >
               <Stack width={"100%"} spacing={"4px"}>
                 <Typography>Нэр</Typography>
-                <TextField placeholder="Нэрээ оруулна уу" />
+                <TextField
+                  name="name"
+                  type="text"
+                  onChange={formikSignUp.handleChange}
+                  value={formikSignUp.values.name}
+                  placeholder="Нэрээ оруулна уу"
+                />
+                {formikSignUp.errors.name && formikSignUp.touched.name ? (
+                  <Typography color={"#EF4444"} sx={{ fontSize: "12px" }}>
+                    {formikSignUp.errors.name}
+                  </Typography>
+                ) : null}
               </Stack>
               <Stack width={"100%"} spacing={"4px"}>
                 <Typography>Имэйл</Typography>
                 <TextField
                   name="email"
-                  type="email"
+                  type="text"
                   onChange={formikSignUp.handleChange}
                   value={formikSignUp.values.email}
                   placeholder="Имэйл хаягаа оруулна уу"
                 />
+                {formikSignUp.errors.email && formikSignUp.touched.email ? (
+                  <Typography color={"#EF4444"} sx={{ fontSize: "12px" }}>
+                    {formikSignUp.errors.email}
+                  </Typography>
+                ) : null}
               </Stack>
               <Stack width={"100%"} spacing={"4px"}>
                 <Typography>Хаяг</Typography>
                 <TextField
                   name="location"
-                  type="location"
+                  type="text"
                   onChange={formikSignUp.handleChange}
                   value={formikSignUp.values.location}
                   placeholder="Та хаягаа оруулна уу"
                 />
+                {formikSignUp.errors.location &&
+                formikSignUp.touched.location ? (
+                  <Typography color={"#EF4444"} sx={{ fontSize: "12px" }}>
+                    {formikSignUp.errors.location}
+                  </Typography>
+                ) : null}
               </Stack>
               <FormControl
                 sx={{ m: 1, width: "100%", display: "flex", gap: "4px" }}
@@ -99,11 +122,10 @@ export const Signup = () => {
                 <Typography>Нууц үг</Typography>
                 <OutlinedInput
                   name="password"
-                  typeof="location"
+                  type="password"
                   onChange={formikSignUp.handleChange}
                   value={formikSignUp.values.password}
                   placeholder="Нууц үгээ оруулна уу"
-                  type={showPassword ? "text" : "password"}
                   endAdornment={
                     <InputAdornment position="end">
                       <IconButton
@@ -117,6 +139,12 @@ export const Signup = () => {
                     </InputAdornment>
                   }
                 />
+                {formikSignUp.errors.password &&
+                formikSignUp.touched.password ? (
+                  <Typography color={"#EF4444"} sx={{ fontSize: "12px" }}>
+                    {formikSignUp.errors.password}
+                  </Typography>
+                ) : null}
               </FormControl>
               <FormControl
                 sx={{ m: 1, width: "100%", display: "flex", gap: "4px" }}
@@ -124,11 +152,10 @@ export const Signup = () => {
                 <Typography>Нууц үг давтах</Typography>
                 <OutlinedInput
                   name="rePassword"
-                  typeof="location"
+                  type="password"
                   onChange={formikSignUp.handleChange}
                   value={formikSignUp.values.rePassword}
                   placeholder="Нууц үгээ оруулна уу"
-                  type={showPassword ? "text" : "password"}
                   endAdornment={
                     <InputAdornment position="end">
                       <IconButton
@@ -141,39 +168,45 @@ export const Signup = () => {
                       </IconButton>
                     </InputAdornment>
                   }
-                  // label="Password"
                 />
+                {formikSignUp.errors.rePassword &&
+                formikSignUp.touched.rePassword ? (
+                  <Typography color={"#EF4444"} sx={{ fontSize: "12px" }}>
+                    {formikSignUp.errors.rePassword}
+                  </Typography>
+                ) : null}
               </FormControl>
             </Stack>
+            <Stack direction={"row"} alignItems={"center"} spacing={"8px"}>
+              <Checkbox
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+                {...label}
+              />
+              <Typography>Үйлчилгээний нөхцөл зөвшөөрөх</Typography>
+            </Stack>
+            <Button
+              type="submit"
+              variant="text"
+              disabled={
+                !formikSignUp.values.name ||
+                !formikSignUp.values.email ||
+                !formikSignUp.values.location ||
+                !formikSignUp.values.password ||
+                !formikSignUp.values.rePassword ||
+                !isChecked
+              }
+              sx={{
+                width: "100%",
+                height: 48,
+                background: "#18BA51",
+                color: "black",
+              }}
+            >
+              Бүртгүүлэх
+            </Button>
           </form>
         </FormikProvider>
-        <Stack direction={"row"} spacing={"8px"}>
-          <CheckBoxIcon></CheckBoxIcon>
-          <Typography>Үйлчилгээний нөхцөл зөвшөөрөх</Typography>
-        </Stack>
-        <Button
-          disabled={
-            formikSignUp.values.email === "" ||
-            !formikSignUp.values.location ||
-            formikSignUp.values.password === "" ||
-            formikSignUp.values.rePassword === ""
-          }
-          variant="text"
-          sx={{
-            width: "100%",
-            height: 48,
-            background:
-              formikSignUp.values.email === "" ||
-              !formikSignUp.values.location ||
-              formikSignUp.values.password === "" ||
-              formikSignUp.values.rePassword === ""
-                ? "#EEEFF2"
-                : "#18BA51",
-            color: "black",
-          }}
-        >
-          Бүртгүүлэх
-        </Button>
       </Stack>
     </>
   );
--- frontend/src/components/utils/context.tsx
+++ frontend/src/components/utils/context.tsx
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useState,
-  useCallback,
-  ReactNode,
-  useContext,
-} from "react";
+import React, { createContext, useState, ReactNode, useContext } from "react";
 
 interface SignUpUserInfo {
   name: string;
@@ -13,8 +7,6 @@ interface SignUpUserInfo {
   location: string;
 }
 interface ContextProps {
-  isLoading: boolean;
-  startLoading: () => void;
   signUpUserInfo: SignUpUserInfo;
   setSignUpUserInfo: (userInfo: SignUpUserInfo) => void;
 }
@@ -27,33 +19,19 @@ const initialUserInfo: SignUpUserInfo = {
 };
 
 const initialContextState: ContextProps = {
-  isLoading: false,
-  startLoading: () => {},
   signUpUserInfo: initialUserInfo,
   setSignUpUserInfo: () => {},
 };
 
 export const UserContext = createContext<ContextProps>(initialContextState);
-
 export const useCustomContext = () => useContext(UserContext);
-
 export const ContextProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [isLoading, setIsLoading] = useState(false);
   const [signUpUserInfo, setSignUpUserInfo] =
     useState<SignUpUserInfo>(initialUserInfo);
 
-  const startLoading = useCallback(() => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, []);
-
   const contextValue: ContextProps = {
-    isLoading,
-    startLoading,
     signUpUserInfo,
     setSignUpUserInfo,
   };
