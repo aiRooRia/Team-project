ae82b958851c96b67411d78e9cd84e88ee7dc7a3
aiRoRia <ariguun10@gmail.com>
2024-03-29 17:35:47 -0700

    3sariin29 formik almost done Ariguun

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-import SignUpSection from "@/components/signup"
-import Footer from "@/components/layout/footer"
-export default function SignUp(){
-    return(
-        <>
-        <SignUpSection/>
-        <Footer/>
-        </>
-    )
-}
-=======
+import Footer from "@/components/layout/footer";
 import { Signup } from "@/components/signup/index";
-const Home = () => {
+export default function SignUp() {
   return (
     <>
       <Signup />
+      <Footer />
     </>
   );
-};
-export default Home;
->>>>>>> a90e605 (3sariin29 button green bolgoh dutuu & formik dutuu Ariguun)
+}
