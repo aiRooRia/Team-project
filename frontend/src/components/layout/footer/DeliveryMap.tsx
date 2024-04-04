d956eeef1f72b539bd2f971b56d200cb98461063
tuvshuu <tuvshee2k@gmail.com>
2024-04-04 19:35:25 +0800

    Added Google Map to Delivery Zone
    Apr.4 Google Map added

---
+++ frontend/src/components/layout/footer/DeliveryMap.tsx
@@ -0,0 +1,11 @@
+export default function DeliveryMap() {
+     return (
+          <>
+               <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d85609.83622562767!2d106.81929953301814!3d47.89173561032449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5d96925be2b18aab%3A0xe606927864a1847f!2sUlaanbaatar!5e0!3m2!1sen!2smn!4v1712229497970!5m2!1sen!2smn"
+                    width="1200"
+                    height="616"
+               ></iframe>
+          </>
+     );
+}
--- frontend/src/pages/layout/delivery-zone.tsx
+++ frontend/src/pages/layout/delivery-zone.tsx
@@ -5,21 +5,36 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
 import { List, ListItem, ListItemText } from "@mui/material";
+import DeliveryMap from "@/components/layout/footer/DeliveryMap";
 
 export default function Home() {
-  const list = [
-    "Нархан хотхон",
-    "26-р байр",
-    "26-р байр",
-    "45-р байр",
-    "45-р байр",
-    "Хоймор хотхон ",
-    "Хоймор хотхон ",
-  ];
-  return (
-    <>
-      <Stack height="59vh" alignItems={"center"} justifyContent={"start"}>
-        {/* <Box
+     const list = [
+          "Нархан хотхон",
+          "26-р байр",
+          "26-р байр",
+          "45-р байр",
+          "45-р байр",
+          "Хоймор хотхон ",
+          "Хоймор хотхон ",
+     ];
+     return (
+          <>
+               <Box
+                    sx={{
+                         display: "flex",
+                         justifyContent: "center",
+                         my: "40px",
+                    }}
+               >
+                    <DeliveryMap />
+               </Box>
+               <Box></Box>
+               <Stack
+                    height="59vh"
+                    alignItems={"center"}
+                    justifyContent={"start"}
+               >
+                    {/* <Box
           sx={{
             width: "1200px",
             height: "100%",
@@ -27,102 +42,183 @@ export default function Home() {
         >
           <img style={{ width: "100%", height: "55%" }} src="/map.png" alt="" />
         </Box> */}
-        <Stack width={"1200px"} alignItems={"start"} justifyContent={"start"}>
-          <Stack
-            direction={"row"}
-            alignItems={"center"}
-            justifyContent={"center"}
-            spacing={1}
-          >
-            <svg
-              width="22"
-              height="22"
-              viewBox="0 0 22 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.18645 2.60351C9.15292 -0.00834179 12.8471 -0.00833988 13.8136 2.60351L14.8427 5.38482C15.1466 6.20598 15.794 6.85341 16.6152 7.15727L19.3965 8.18645C22.0083 9.15292 22.0083 12.8471 19.3965 13.8136L16.6152 14.8427C15.794 15.1466 15.1466 15.794 14.8427 16.6152L13.8136 19.3965C12.8471 22.0083 9.15292 22.0083 8.18644 19.3965L7.15727 16.6152C6.85341 15.794 6.20598 15.1466 5.38482 14.8427L2.60351 13.8136C-0.00834179 12.8471 -0.00833988 9.15292 2.60351 8.18645L5.38482 7.15727C6.20598 6.85341 6.85341 6.20598 7.15727 5.38482L8.18645 2.60351Z"
-                fill="#18BA51"
-              />
-            </svg>
-            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-              Хүргэлтийн бүс дэх хаягууд
-            </Typography>
-          </Stack>
-          <Stack
-            width={"100%"}
-            direction={"row"}
-            justifyContent={"space-between"}
-          >
-            <Card sx={{ width: "49%" }}>
-              <CardActionArea>
-                {/* <CardMedia
+                    <Stack
+                         width={"1200px"}
+                         alignItems={"start"}
+                         justifyContent={"start"}
+                    >
+                         <Stack
+                              direction={"row"}
+                              alignItems={"center"}
+                              justifyContent={"center"}
+                              spacing={1}
+                         >
+                              <svg
+                                   width="22"
+                                   height="22"
+                                   viewBox="0 0 22 22"
+                                   fill="none"
+                                   xmlns="http://www.w3.org/2000/svg"
+                              >
+                                   <path
+                                        d="M8.18645 2.60351C9.15292 -0.00834179 12.8471 -0.00833988 13.8136 2.60351L14.8427 5.38482C15.1466 6.20598 15.794 6.85341 16.6152 7.15727L19.3965 8.18645C22.0083 9.15292 22.0083 12.8471 19.3965 13.8136L16.6152 14.8427C15.794 15.1466 15.1466 15.794 14.8427 16.6152L13.8136 19.3965C12.8471 22.0083 9.15292 22.0083 8.18644 19.3965L7.15727 16.6152C6.85341 15.794 6.20598 15.1466 5.38482 14.8427L2.60351 13.8136C-0.00834179 12.8471 -0.00833988 9.15292 2.60351 8.18645L5.38482 7.15727C6.20598 6.85341 6.85341 6.20598 7.15727 5.38482L8.18645 2.60351Z"
+                                        fill="#18BA51"
+                                   />
+                              </svg>
+                              <Typography
+                                   variant="h6"
+                                   sx={{ fontWeight: "bold" }}
+                              >
+                                   Хүргэлтийн бүс дэх хаягууд
+                              </Typography>
+                         </Stack>
+                         <Stack
+                              width={"100%"}
+                              direction={"row"}
+                              justifyContent={"space-between"}
+                         >
+                              <Card sx={{ width: "49%" }}>
+                                   <CardActionArea>
+                                        {/* <CardMedia
           component="img"
           height="140"
           image="/static/images/cards/contemplative-reptile.jpg"
           alt="green iguana"
         /> */}
-                <CardContent sx={{ margin: 3 }}>
-                  <Typography gutterBottom variant="h5" component="div">
-                    А бүс
-                  </Typography>
-                  <Divider sx={{ borderColor: "green" }}></Divider>
-                  <Stack direction={"row"} justifyContent={"space-between"}>
-                    <List sx={{ width: "45%" }}>
-                      {list.map((el, index) => (
-                        <ListItem key={index} sx={{ padding: 0 }}>
-                          <ListItemText primary={el} />
-                        </ListItem>
-                      ))}
-                    </List>
-                    <List sx={{ width: "45%" }}>
-                      {list.map((el, index) => (
-                        <ListItem key={index} sx={{ padding: 0 }}>
-                          <ListItemText primary={el} />
-                        </ListItem>
-                      ))}
-                    </List>
-                  </Stack>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-            <Card sx={{ width: "49%" }}>
-              <CardActionArea>
-                {/* <CardMedia
+                                        <CardContent sx={{ margin: 3 }}>
+                                             <Typography
+                                                  gutterBottom
+                                                  variant="h5"
+                                                  component="div"
+                                             >
+                                                  А бүс
+                                             </Typography>
+                                             <Divider
+                                                  sx={{ borderColor: "green" }}
+                                             ></Divider>
+                                             <Stack
+                                                  direction={"row"}
+                                                  justifyContent={
+                                                       "space-between"
+                                                  }
+                                             >
+                                                  <List sx={{ width: "45%" }}>
+                                                       {list.map(
+                                                            (el, index) => (
+                                                                 <ListItem
+                                                                      key={
+                                                                           index
+                                                                      }
+                                                                      sx={{
+                                                                           padding: 0,
+                                                                      }}
+                                                                 >
+                                                                      <ListItemText
+                                                                           primary={
+                                                                                el
+                                                                           }
+                                                                      />
+                                                                 </ListItem>
+                                                            )
+                                                       )}
+                                                  </List>
+                                                  <List sx={{ width: "45%" }}>
+                                                       {list.map(
+                                                            (el, index) => (
+                                                                 <ListItem
+                                                                      key={
+                                                                           index
+                                                                      }
+                                                                      sx={{
+                                                                           padding: 0,
+                                                                      }}
+                                                                 >
+                                                                      <ListItemText
+                                                                           primary={
+                                                                                el
+                                                                           }
+                                                                      />
+                                                                 </ListItem>
+                                                            )
+                                                       )}
+                                                  </List>
+                                             </Stack>
+                                        </CardContent>
+                                   </CardActionArea>
+                              </Card>
+                              <Card sx={{ width: "49%" }}>
+                                   <CardActionArea>
+                                        {/* <CardMedia
           component="img"
           height="140"
           image="/static/images/cards/contemplative-reptile.jpg"
           alt="green iguana"
         /> */}
-                <CardContent sx={{ margin: 3 }}>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Б бүс
-                  </Typography>
-                  <Divider sx={{ borderColor: "green" }}></Divider>
-                  <Stack direction={"row"} justifyContent={"space-between"}>
-                    <List sx={{ width: "45%" }}>
-                      {list.map((el, index) => (
-                        <ListItem key={index} sx={{ padding: 0 }}>
-                          <ListItemText primary={el} />
-                        </ListItem>
-                      ))}
-                    </List>
-                    <List sx={{ width: "45%" }}>
-                      {list.map((el, index) => (
-                        <ListItem key={index} sx={{ padding: 0 }}>
-                          <ListItemText primary={el} />
-                        </ListItem>
-                      ))}
-                    </List>
-                  </Stack>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Stack>
-        </Stack>
-      </Stack>
-      <Footer />
-    </>
-  );
+                                        <CardContent sx={{ margin: 3 }}>
+                                             <Typography
+                                                  gutterBottom
+                                                  variant="h5"
+                                                  component="div"
+                                             >
+                                                  Б бүс
+                                             </Typography>
+                                             <Divider
+                                                  sx={{ borderColor: "green" }}
+                                             ></Divider>
+                                             <Stack
+                                                  direction={"row"}
+                                                  justifyContent={
+                                                       "space-between"
+                                                  }
+                                             >
+                                                  <List sx={{ width: "45%" }}>
+                                                       {list.map(
+                                                            (el, index) => (
+                                                                 <ListItem
+                                                                      key={
+                                                                           index
+                                                                      }
+                                                                      sx={{
+                                                                           padding: 0,
+                                                                      }}
+                                                                 >
+                                                                      <ListItemText
+                                                                           primary={
+                                                                                el
+                                                                           }
+                                                                      />
+                                                                 </ListItem>
+                                                            )
+                                                       )}
+                                                  </List>
+                                                  <List sx={{ width: "45%" }}>
+                                                       {list.map(
+                                                            (el, index) => (
+                                                                 <ListItem
+                                                                      key={
+                                                                           index
+                                                                      }
+                                                                      sx={{
+                                                                           padding: 0,
+                                                                      }}
+                                                                 >
+                                                                      <ListItemText
+                                                                           primary={
+                                                                                el
+                                                                           }
+                                                                      />
+                                                                 </ListItem>
+                                                            )
+                                                       )}
+                                                  </List>
+                                             </Stack>
+                                        </CardContent>
+                                   </CardActionArea>
+                              </Card>
+                         </Stack>
+                    </Stack>
+               </Stack>
+               <Footer />
+          </>
+     );
 }
