3d39e3b07f36d47bfecd0d516a0978fd487f6c30
Zulsar199 <zulsar199@gmail.com>
2024-04-05 12:02:48 +0700

    comment arilgasn

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,18 +1,3 @@
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
-// import { UserContextProvider } from "@/components/utils/context/userContext";
-// import { Header } from "@/components/layout/header";
-// import { AdminContextProvider } from "@/components/utils/context/adminContext";
-// export default function App({ Component, pageProps }: AppProps) {
-//   return (
-//     <AdminContextProvider>
-//       <UserContextProvider>
-//         <Header></Header>
-//         <Component {...pageProps} />
-//       </UserContextProvider>
-//     </AdminContextProvider>
-//   );
-// }
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
